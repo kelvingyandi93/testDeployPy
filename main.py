ac4ea7af998c9d445330da1169d05cd46b4d0487
Kelvin Gyandi <76482131+kelvingyandi93@users.noreply.github.com>
2023-11-08 09:29:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -221,4 +221,4 @@ async def predict(file: UploadFile):
 
 # Run the server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
